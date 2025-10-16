d159ff09e58abe783d4efa64bfa7ca5b53c8457f
Thalion-Sec <nlassonpro@gmail.com>
2025-10-16 09:52:34 +0200

    First

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Bienvenue sur ma première application web en Python 🚀"
+
+@app.route("/hello/<name>")
+def hello(name):
+    return f"Salut {name}, content de te voir ici ! 😄"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
